a4308b868017d466b51753b15390c0f5c3cca673
csdevs1 <csdevs1@gmail.com>
2017-02-28 16:59:19 -0300

    Download monthly UF rate as a XLS file

--- application/controllers/Dsy_controller.php
+++ application/controllers/Dsy_controller.php
@@ -21,7 +21,7 @@ class Dsy_controller extends CI_Controller{
         //$date=explode('-',$this->input->post('date'));
         //$json = json_decode(file_get_contents("http://api.sbif.cl/api-sbifv3/recursos_api/uf/".$date[0]."/".$date[1]."?apikey=d8093171162117c0c6e8da895b00978d4e2b6a0e&formato=json"),true);
         $json = json_decode(file_get_contents("http://api.sbif.cl/api-sbifv3/recursos_api/uf/2010/02?apikey=d8093171162117c0c6e8da895b00978d4e2b6a0e&formato=json"),true);
-        $this->export_excel->to_excel($json,'lista_uf');
+        $this->export_excel->to_excel($json['UFs'],'lista_uf');
     }
 }
 
